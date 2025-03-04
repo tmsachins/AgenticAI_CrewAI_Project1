89c3b0d9f8784ed2f7c0ce2f724060d3a764671c
Sachin S <tmsachins@gmail.com>
2025-03-03 18:35:41 -0600

    Created research and writer agent

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 print(os.environ["OPENAI_API_KEY"])
 search_tool = SerperDevTool()
 
+topic = "quantum computing"
+
 # Creating a senior researcher agent with memory and verbose mode
 research_agent = Agent(
     role="Senior Researcher",
@@ -25,4 +27,19 @@ research_agent = Agent(
     ),
     tools=[search_tool],
     allow_delegation=True
+)
+
+# Creating a writer agent with custom tools and delegation capability
+writer = Agent(
+    role="Writer",
+    goal="Narrate compelling tech stories about {topic}",
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        "engaing narratives that captivate and educate, bringing new"
+        "discoveries to light in an accessible manner."
+    ),
+    tools=[search_tool],
+    allow_delegation=False
 )
